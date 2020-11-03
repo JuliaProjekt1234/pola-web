03658fd6d9564f071609b75fac6f1737bb49caee
JuliaProjekt1234 <juliadudzinska1996@gmail.com>
2020-11-03 09:09:59 +0100

    dodanie modalu, wyglad serach inputa

--- src/pages/serach/Search.js
+++ src/pages/serach/Search.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import SearchModal from './SearchModal';
 import axios from 'axios'
+import { InputGroup, FormControl, Button, Modal } from "react-bootstrap"
+import { SearchButton, SearchFormControl, SearchInputGroup } from "./Search.css"
+import { ImSearch } from 'react-icons/im'
 
 const Search = () => {
   const [ean, setEan] = useState("")
@@ -28,12 +31,12 @@ const Search = () => {
       const id = randomDeviceId();
       localStorage.setItem('DEVICE_ID', id);
       setDeviceId(id)
-      
-    }    
+
+    }
   }, []);
 
-  const openModal = () => setOpen({ isOpen: true });
-  // const closeModal = () => setOpen({ isOpen: false });
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -43,7 +46,6 @@ const Search = () => {
           params: {
             code: ean,
             device_id: deviceId
-
           }
         }
       )
@@ -58,19 +60,54 @@ const Search = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text"
-        value={ean}
-        onChange={e => setEan(e.target.value)}
-      />
-      <button type="submit">Sprawdź</button>
-      {
+    <div style={{ width: '100%' }}>
+      <form style={{ width: '50%', margin: '0 auto' }} onSubmit={handleSubmit}>
+        <InputGroup className="mb-3 border-0 w-100" >
+          <SearchFormControl
+            type='text'
+            value={ean}
+            onChange={e => setEan(e.target.value)}
+            placeholder="Wpisz tutaj kod kreskowy"
+          />
+          <InputGroup.Append>
+            <SearchButton type='submit' variant="outline-secondary">
+              <ImSearch />
+            </SearchButton>
+          </InputGroup.Append>
+        </InputGroup>
+      </form>
+      <Modal
+        size="lg"
+        show={isOpen}
+        onHide={closeModal}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Large Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SearchModal
+            data={data}
+          />
+        </Modal.Body>
+      </Modal>
+      {/* {
         isOpen &&
         <SearchModal
           data={data}
         />
-      }
-    </form>
+      } */}
+      {/* <form>
+        <input type="text"
+          value={ean}
+          onChange={e => setEan(e.target.value)}
+        />
+        <button type="submit">Sprawdź</button>
+     
+      </form> */}
+    </div >
   )
 }
 
